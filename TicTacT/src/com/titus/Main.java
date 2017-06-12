930a9630d4f6ed7768c1c3cf5189c0fc9d8272a5
Titus Treme <tremetitus@gmail.com>
2017-06-12 14:16:34 -0600

    Working

---
+++ TicTacT/src/com/titus/Main.java
@@ -0,0 +1,14 @@
+package com.titus;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String myStr;
+
+//    array to hold X or O in places
+    public static char[] place = {'_','_','_','_','_','_','_','_','_'};
+
+    public static void main(String[] args) {
+
+    }
+}
